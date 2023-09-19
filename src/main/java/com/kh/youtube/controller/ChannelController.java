3f7d36e495b0ca06eccd3cb86ad716d566e1dc90
dclown13 <w6092w@gmail.com>
2023-09-19 17:42:20 +0900

    09-19

--- src/main/java/com/kh/youtube/controller/ChannelController.java
+++ src/main/java/com/kh/youtube/controller/ChannelController.java
@@ -15,8 +15,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/*")
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
 public class ChannelController {
-    
+
     @Autowired
     private ChannelService channel;
 
@@ -25,27 +26,27 @@ public class ChannelController {
 
     @Autowired
     private SubscribeService subscribe;
-    
-    
+
+
     // 채널 조회 : GET - http://localhost:8080/api/channel/1
-    @GetMapping("/channer/{id}")
+    @GetMapping("/channel/{id}")
     public ResponseEntity<Channel> showChannel(@PathVariable int id) {
         return ResponseEntity.status(HttpStatus.OK).body(channel.show(id));
     }
 
     // 채널에 있는 영상 조회 : GET - http://localhost:8080/api/channel/1/video
-    @GetMapping("/channer/{id}")
+    @GetMapping("/channel/1/{id}")
     public ResponseEntity<List<Video>> channelVideoList(@PathVariable int id) {
         // SELECT * FROM video WHERE channel_code = ?
         return ResponseEntity.status(HttpStatus.OK).body(video.findByChannelCode(id));
     }
-    
+
     // 채널 추가 : POST - http://localhost:8080/api/channel
     @PostMapping("/channel")
     public ResponseEntity<Channel> createChannel(@RequestBody Channel vo) {
         return ResponseEntity.status(HttpStatus.OK).body(channel.create(vo));
     }
-    
+
     // 채널 수정 : PUT - http://localhost:8080/api/channel
 
     @PutMapping("/channel")
@@ -59,7 +60,7 @@ public class ChannelController {
     public ResponseEntity<Channel> deleteChannel(@PathVariable int id) {
         return ResponseEntity.status(HttpStatus.OK).body(channel.delete(id));
     }
-    
+
     // 내가 구독한 채널 조회 : GET - http://localhost:8080/api/subscribe/user1
 
     @GetMapping("/subscribe/{id}")
@@ -72,7 +73,7 @@ public class ChannelController {
     public ResponseEntity<Subscribe> createSubscribe(@RequestBody Subscribe vo) {
         return ResponseEntity.status(HttpStatus.OK).body(subscribe.create(vo));
     }
-    
+
     // 채널 구독 취소 : DELETE - http://localhost:8080/api/subscribe/1
     @DeleteMapping("/subscribe/{id}")
     public ResponseEntity<Subscribe> delete(@PathVariable int id) {
