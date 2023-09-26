ca8df845ec3013b745b228ed8aa83c2490339e33
dclown13 <w6092w@gmail.com>
2023-09-26 15:29:04 +0900

    09-26

--- src/main/java/com/kh/youtube/repo/MemberDAO.java
+++ src/main/java/com/kh/youtube/repo/MemberDAO.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberDAO extends JpaRepository<Member, String> {
 
-
+    Member findByIdAndPassword(String id, String password);
 
 }
